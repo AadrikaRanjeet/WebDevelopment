b0cbd931fc422be86808632215ba7b63baa55982
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-06 08:24:35 +0530

    Add files via upload

---
+++ JQuery/ManipulatingStylesUsingJquery.js
@@ -0,0 +1,7 @@
+alert("working");
+//adding styles with the help of class 
+$("h1").addClass("big-title");
+//can add multiple classes to the single element 
+$("button").addClass("big-title margin-50");
+//to check if the element has that class-will give o/p as true or false
+$("h1").hasClass("margin-50");
