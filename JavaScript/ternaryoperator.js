184ebc57e4a9ee442f47d5d103d297f9e2dfb3f4
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-10 01:37:34 +0530

    Add files via upload

---
+++ JavaScript/ternaryoperator.js
@@ -0,0 +1,5 @@
+//ternary operator is a short cut of writing if else statement 
+
+var age=18;
+//if age is less than 18 print cant drive else print drive 
+console.log(age<18? "cant drive":"drive")
