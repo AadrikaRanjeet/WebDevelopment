8a09f84951452995f04291d0520cca37995b7a2d
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-10 01:48:51 +0530

    Add files via upload

---
+++ JavaScript/switchCase.js
@@ -0,0 +1,15 @@
+let age=prompt("What is your age")
+switch (age) {
+    case '13':
+        console.log("age is "+age)
+        break;
+        case '14':
+            console.log("age is "+age)
+            break;
+            case '15':
+        console.log("age is special"+age)
+        break;
+    default:
+        console.log("Not special")
+        break;
+}
