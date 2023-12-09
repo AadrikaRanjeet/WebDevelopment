183b327162bb792f8049684893aaa69ed353faca
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-10 02:50:27 +0530

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,12 @@
+const Function =() =>
+{
+    console.log("Hello");
+}
+Function();//calling or invoking  function
+const Sum =(a,b) =>
+{
+    return a+b;
+}
+
+var sum=Sum(9,10);
+console.log(sum);
