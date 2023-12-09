da1936c923e4ead2ad5bc98eccb6f530cfd82dcf
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-10 01:40:06 +0530

    Update ternaryoperator.js

--- JavaScript/ternaryoperator.js
+++ JavaScript/ternaryoperator.js
@@ -1,5 +1,5 @@
 //ternary operator is a short cut of writing if else statement 
-
+//condition? expression1:expression2(else part )
 var age=18;
 //if age is less than 18 print cant drive else print drive 
 console.log("You",age<18? "cant drive":"drive")
