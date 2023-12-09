ccd4ea165196b697764feb045127e62ebdfa26a5
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-10 03:18:57 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,10 @@
+let boy1="pramod"
+let boy2="Nikhil"
+//template literals
+console.log(`${boy1} is a friend of ${boy2}`)
+
+console.log(`The name "boy1" is 'Harry'`)
+
+//String interpolation :- you can directly insert variable using template literal
+var a=`"This is ${boy1}"`
+console.log(a)
