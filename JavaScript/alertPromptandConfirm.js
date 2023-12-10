49eb07deb04afa1b37531e4b8731deff2b6cead8
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-11 01:51:59 +0530

    Add files via upload

---
+++ JavaScript/alertPromptandConfirm.js
@@ -0,0 +1,5 @@
+alert("Enter a value") //alert : used to invoke a mini window with msg
+var b=prompt("Enter value here");// prompt : used tio take user input
+var ab=prompt("Enter value here",89);//second parameter is the default value 
+let write =confirm("Do you want to write it on the page")//confirm : shows a msg and waits for user to press ok or cancel ,returns true for ok and false for cance
+
