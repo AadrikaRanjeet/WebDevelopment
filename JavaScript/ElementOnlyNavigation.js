a316435bf3adcac182031cd41a83a567a5f2a487
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-11 12:32:56 +0530

    Add files via upload

---
+++ JavaScript/ElementOnlyNavigation.js
@@ -0,0 +1,8 @@
+
+const Fun=()=>{
+    document.body.firstElementChild.style.background="red"
+}
+var b=document.body
+console.log("First child of b is :",b.firstChild)
+console.log("First child of b is :",b.firstElementChild)
+
