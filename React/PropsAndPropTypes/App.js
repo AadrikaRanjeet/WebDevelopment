0b9eda02b7360010c8e5c492f52820a96b22ce81
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-13 01:52:44 +0530

    Add files via upload

---
+++ React/PropsAndPropTypes/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+let name="Aadrika";
+function App() {
+  return (
+    <>
+      <p>{name}</p>
+     <Navbar title="TectUtils" 
+     abc="Hello"/>
+    </>
+  );
+}
+
+export default App;
