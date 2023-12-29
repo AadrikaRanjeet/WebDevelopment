e5d7a3232299ab75157df2090aae11f62bc13e1a
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-12-29 18:52:15 +0530

    Add files via upload
    In this file  we have written a class based component syntax

---
+++ React/ClassBasedComponents/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello , My first class based component
+      </div>
+    )
+  }
+}
+
+
+
